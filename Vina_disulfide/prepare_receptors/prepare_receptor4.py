e5bed7d67b0e84d3ac9ea927fdb76740590dbd3d
Demian Riccardi <demianriccardi@gmail.com>
2014-08-07 19:59:13 -0400

    Update prepare_receptor4.py

--- Vina_disulfide/prepare_receptors/prepare_receptor4.py
+++ Vina_disulfide/prepare_receptors/prepare_receptor4.py
@@ -1,6 +1,4 @@
 #!/usr/bin/env /Library/MGLTools/latest/bin/pythonsh
 # THIS IS JUST A PLACE HOLDER FOR THE SCRIPT!  To get the script, please install MGLTools
 # 
-#
-# $Header: /opt/cvs/python/packages/share1.5/AutoDockTools/Utilities24/prepare_receptor4.py,v 1.14 2014/01/30 17:39:23 rhuey Exp $
-#
+# the path in the shebang line is where it is on my OS X machine
